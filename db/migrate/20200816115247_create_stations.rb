33c2080cddb220c6bae0b3f89864dfcbc619ff13
uratomo <rop.ibuki@gmail.com>
2020-08-16 21:03:49 +0900

    station　class作成

---
+++ db/migrate/20200816115247_create_stations.rb
@@ -0,0 +1,11 @@
+class CreateStations < ActiveRecord::Migration[5.0]
+  def change
+    create_table :stations do |t|
+      t.references :building, foreign_key: true
+      t.string :name
+      t.integer :walk
+
+      t.timestamps
+    end
+  end
+end
